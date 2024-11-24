16a1b26ef84d90a224134b73d9fa2b99a6bb74bd
Nipuna7 <nipunanirodha@gmail.com>
2024-11-24 17:50:04 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+import BodyContent from './Components/BodyContent/BodyContent';
+import FooterContent from './Components/FooterContent/FooterContent';
+import HeaderContent from './Components/HeaderContent/HeaderContent';
+
+
+function App() {
+
+  return (
+    <>
+     <div id='wrapper'>
+      <HeaderContent/>
+      <BodyContent/>
+      <FooterContent/>
+     </div>
+    </>
+   
+  )
+}
+
+export default App;
---
+++ src/Components/BodyContent/BodyContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BodyContent.css'
+
+
+function BodyContent() {
+  return (
+    <>
+    <div id='bodycontent'>
+        <h3>Main Content</h3>
+      </div>
+      </>
+  )
+}
+
+export default BodyContent
